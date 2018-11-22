fc7f8d8933f878a3e9956ea8d975bb4c1cab4315
letrungkien211 <letrungkien211@gmail.com>
2018-11-23 00:03:47 +0900

    commit code

---
+++ src/SfNotFoundMiddleWare.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SfNotFound
+{
+    /// <summary>
+    /// Refer to https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-reverseproxy  Keyword: X-ServiceFabric
+    /// </summary>
+    internal class SfNotFoundMiddleWare
+    {
+        private readonly RequestDelegate _next;
+
+        public SfNotFoundMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+            if (httpContext.Response.StatusCode == 404)
+            {
+                httpContext.Response.Headers["X-ServiceFabric"] = "ResourceNotFound";
+            }
+        }
+    }
+}
---
+++ src/SfNotFoundMiddleWareExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace SfNotFound
+{
+    /// <summary>
+    /// Extension method used to add the middleware to the HTTP request pipeline.
+    /// </summary>
+    public static class SfNotFoundMiddleWareExtensions
+    {
+        /// <summary>
+        /// Use Sf Not Found MiddleWare
+        /// Refer to https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-reverseproxy  Keyword: X-ServiceFabric
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseSfNotFoundMiddleWare(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SfNotFoundMiddleWare>();
+        }
+    }
+}
